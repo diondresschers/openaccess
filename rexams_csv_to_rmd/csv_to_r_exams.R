1a3604497c1706a423fa582a886e50197dc27a04
Dion Dresschers <dion@dresschers.net>
2021-08-16 17:51:27 +0200

    some changes

---
+++ rexams_csv_to_rmd/csv_to_r_exams.R
@@ -0,0 +1,19 @@
+library("exams")
+
+myexam <- list (
+  "question_0006.Rmd"
+  #"question_0003.Rmd",
+  # "question_0001.Rmd",
+#  "question_0002.Rmd",
+#  "question_0003.Rmd",
+#  "question_0004.Rmd",
+#  "question_0005.Rmd"
+)
+#exams2html(myexam)
+#exams2canvas(myexam)
+exams2testvision(myexam)
+#exams2nops(myexam)
+
+
+
+
